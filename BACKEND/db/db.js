1dbb982200a2cf710e130f7a9c05682760d85573
Shivamgupta0101 <shivamgupta819188@gmail.com>
2026-04-03 09:35:01 +0530

    Implement MongoDB connection in db.js
    Add connection logic to MongoDB using MONGODB_URI

--- BACKEND/db/db.js
+++ BACKEND/db/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import 'dotenv/config'
 function connect() {
+    
     mongoose.connect(process.env.MONGODB_URI).then(() => {
+        // .then() runs when the promise is successfully resolved asynchronously
         console.log("Connected to MongoDB");
     }).catch(err => {
         console.log(err);
